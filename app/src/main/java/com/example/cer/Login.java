bf3ec8924d313c66acc4a155f8c1fc5b392b3b26
Chrisandres <Chrisandres6@gmail.com>
2019-12-09 21:09:56 -0300

    Adding new layouts (empty)

--- app/src/main/java/com/example/cer/Login.java
+++ app/src/main/java/com/example/cer/Login.java
@@ -2,13 +2,28 @@ package com.example.cer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView tvRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvRegistro = (TextView)findViewById(R.id.tvRegistro);
+        tvRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),UserTypeCreation.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
