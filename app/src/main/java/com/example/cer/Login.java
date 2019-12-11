ca3aa99e5970c0624854f7236a4bbcc59303f5bf
Chrisandres <chrisandres6@gmail.com>
2019-12-11 16:25:53 -0300

    Configuring first main layouts

--- app/src/main/java/com/example/cer/Login.java
+++ app/src/main/java/com/example/cer/Login.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
     TextView tvRegistro;
+    Button btnIngresarCliente;
+    Button btnIngresarColectivo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,24 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),UserTypeCreation.class);
+                startActivity(i);
+            }
+        });
+
+        btnIngresarCliente = (Button)findViewById(R.id.btnIngresarCliente);
+        btnIngresarCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainCliente.class);
+                startActivity(i);
+            }
+        });
+
+        btnIngresarColectivo = (Button)findViewById(R.id.btnIngresarColectivo);
+        btnIngresarColectivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainColectivo.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/cer/MainColectivo.java
+++ app/src/main/java/com/example/cer/MainColectivo.java
@@ -2,9 +2,43 @@ package com.example.cer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainColectivo extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id =item.getItemId();
+
+        Context context = getApplicationContext();
+        CharSequence text = null;
+        int duration = Toast.LENGTH_SHORT;
+
+        if(id == R.id.CerrarSesion){
+            text = "Ha cerrado su sesión";
+            Intent actionC = new Intent(MainColectivo.this,Login.class);
+            actionC.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(actionC);
+            finish();
+        }
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
